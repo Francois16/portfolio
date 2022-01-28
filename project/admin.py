6279b30cd1b2bdd778ea2ea967cd7d4d0ba620f9
Francois <Fpaul1274@gmail.com>
2022-01-28 07:35:20 +0200

    registered the Project, Category and Tag models

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import Project, Category, Tag
+
+admin.site.register(Project)
+admin.site.register(Category)
+admin.site.register(Tag)
