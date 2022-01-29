5044e4dc93b0a1fc5ceabc6ed46db94bd67c6767
Francois <Fpaul1274@gmail.com>
2022-01-29 11:47:10 +0200

    Added contact form to context

--- page/views.py
+++ page/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Models
 from project.models import Project
 
+# Forms
+from contact.forms import ContactForm
+
 
 def indexview(request):
     template_name = "page/index.html"
@@ -11,6 +14,7 @@ def indexview(request):
 
     context = {
         "projects": projects,
+        "contact_form": ContactForm,
     }
 
     return render(request, template_name, context)
