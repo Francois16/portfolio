235e5c74410dfbe41c960c63204f21ec43c86c48
Francois <Fpaul1274@gmail.com>
2022-01-29 11:44:55 +0200

    Added Contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+"""
+Contact
+    - first_name
+    - last_name
+    - email (Unique = True)
+    - phone_number
+    - is_subscribed
+"""
+
+
+class Contact(models.Model):
+    class Meta:
+        ordering = ["first_name"]
+        verbose_name_plural = "Contacts"
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=14)
+    is_subscribed = models.BooleanField()
+
+    def __str__(self):
+        return self.email
+
+    def get_absolute_url(self):
+        return reverse("index")
