ddd6ef5cf38a6f47e65c3174d0d33f493a34e282
Francois <Fpaul1274@gmail.com>
2022-01-30 10:00:03 +0200

    added another test in projects

--- project/tests.py
+++ project/tests.py
@@ -40,3 +40,21 @@ class ProjectTests(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, self.project.title)
         self.assertTemplateUsed(resp, "page/index.html")
+
+    def test_same_tag_and_category_can_be_used_more_than_once(self):
+        self.project2 = Project.objects.create(
+            title="Oaks on Main Shopping Center",
+            url="www.oaksonmain.co.za",
+            image=SimpleUploadedFile(
+                name="test.jpg",
+                content=b"a",
+                content_type="image/jpeg",
+            ),
+            description="Beautiful website created for Oaks on Main Shopping Center in Knysna!",
+            category=Category.objects.get(name="Website"),
+        )
+
+        self.project2.tags.add(Tag.objects.get(name="HTML5"))
+
+        self.assertEqual(self.project.category, self.project2.category)
+        self.assertEqual(list(self.project.tags.all()), list(self.project2.tags.all()))
